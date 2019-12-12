0b7c00cbf5095afd2e05d1a0d58624fb2e1f0aae
Tom Klaver <tomklav@gmail.com>
2019-12-12 17:23:24 +0100

    adds include_package_data=True,

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/tadpole-share/tadpole-algorithms",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
       'numpy',
       'pandas',
