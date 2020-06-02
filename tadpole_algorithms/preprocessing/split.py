aae9d4d249e699e0d82360b8ab6a5c6bcbe3d69e
Lotte Mulder <LotteMulder1503@gmail.com>
2020-06-02 15:20:06 +0200

    added spit function for testing with D3

--- tadpole_algorithms/preprocessing/split.py
+++ tadpole_algorithms/preprocessing/split.py
@@ -1,8 +1,12 @@
 import numpy as np
 
-def split_test_train(df_train_test, df_eval, random_seed=0):
+#def split_test_train_random
+
+
+def split_test_train_tadpole(df_train_test, df_eval, random_seed=0):
     """
-    Split a dataframe into three parts: train, test & evaluation
+    Split dataframes into three parts: train, test & evaluation
+    These are the sets as used in challenge evaluation for the paper Marinescu et al, 2020, ArXiv
     Train: patients (RIDs) from D1,D2 ADNI Data sets
     Test: roll-over patients (RIDs) from D1,D2 ADNI Data sets that are in D4
     Eval: D4 ADNI Data set
@@ -25,3 +29,32 @@ def split_test_train(df_train_test, df_eval, random_seed=0):
 
     return train_df, test_df, eval_df
 
+
+def split_test_train_D3(df_train, df_test, df_eval, random_seed=0)
+    """
+    Split a dataframe into three parts: train, test and evaluation
+    Data set used for train is D1D2, for test D3 and D4 for evalution
+    Cross sectional dataset is used as test set
+
+    """
+    # get only patient IDs with at least 2 rows per patient (required for test/eval set)
+    ids = df_train_test.groupby('RID').filter(lambda x: len(x) > 1)['RID'].unique()
+    
+    train_df = df_train_test[df_train_test['RID'].isin(ids)]
+
+    # get D1 rows only
+    train_df = train_df.loc[train_df['D2'] == 1]
+
+    # select all records where RID is in d4.
+    test_df = df_test[
+        df_test['RID'].isin(df_eval['RID'].unique())
+    ]
+
+    eval_df = df_eval
+
+    return train_df, test_df, eval_df
+
+
+    # def split_test_train_parelsnoer(df_train_test, df_eval, random_seed=0):
+    #     #### Will be written soon
+    
