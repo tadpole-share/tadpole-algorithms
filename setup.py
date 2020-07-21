06f5bdc27fe60ec906266d476748148992ef694b
Carlos Martinez <c.martinez@esciencecenter.nl>
2020-07-21 17:05:58 +0200

    Update setup to harvest requirements

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 import setuptools
+import codecs
+import os
+
+# Collect requirements from all requirements.txt files
+install_requires = set()
+for root,dirs,files in os.walk('.'):
+    if 'requirements.txt' in files:
+        reqs_file = os.path.join(root, 'requirements.txt')
+        with codecs.open(reqs_file, mode='r') as f:
+            reqs = f.read().splitlines()
+            install_requires.update(reqs)
 
 setuptools.setup(
     name="tadpole_algorithms",
     version="1.0.0",
     author="Eyra team",
-    author_email="t.klaver@esciencecenter.nl",
+    author_email="c.martinez@esciencecenter.nl",
     description="Tadpole algorithms",
     long_description="Tadpole algorithms",
     long_description_content_type="text/markdown",
     url="https://github.com/tadpole-share/tadpole-algorithms",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=[
-      'numpy',
-      'pandas',
-      'scikit-learn',
-      'scipy',
-      'tqdm'
-    ],
+    install_requires=list(install_requires),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache 2.0",
