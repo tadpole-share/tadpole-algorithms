0d97ebda3b0bfbbb31f33b3878b77ef1f37c5524
Esther Bron <e.bron@erasmusmc.nl>
2020-07-23 08:48:08 +0200

    Fixed 2 bugs in split_train_test_tadpole: D2 test set should not select last timepoint but all; for D3 train set should only consist of D1 (and not D2) rids

--- tadpole_algorithms/preprocessing/split.py
+++ tadpole_algorithms/preprocessing/split.py
@@ -18,8 +18,8 @@ def split_test_train_tadpole(df_train_test, df_eval, random_seed=0):
     
     train_df = df_train_test[df_train_test['RID'].isin(ids)]
         
-    # get last row per RID
-    df_train_test = df_train_test.groupby('RID').tail(1)
+    ## get last row per RID
+    #df_train_test = df_train_test.groupby('RID').tail(1)
 
     # select all records where RID is in d4.
     test_df = df_train_test[
@@ -44,7 +44,7 @@ def split_test_train_d3(df_train, df_test, df_eval, random_seed=0):
     train_df = df_train[df_train['RID'].isin(ids)]
 
     # get D1 rows only
-    train_df = train_df.loc[train_df['D2'] == 1]
+    train_df = train_df.loc[train_df['D2'] == 0]
 
     # select all records where RID is in d4.
     test_df = df_test[
